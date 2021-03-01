779ca92f28de11b6f48c0ed58f927dc333995942
Havishay Cloud <79502798+Havishay-Cloud@users.noreply.github.com>
2021-03-01 11:29:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-  var firebaseConfig = {
-    apiKey: "AIzaSyAQP3LepTFSgBhm0YXD5eH-Z9fnIPEQB9s",
-    authDomain: "uploadsfromsite.firebaseapp.com",
-    projectId: "uploadsfromsite",
-    storageBucket: "uploadsfromsite.appspot.com",
-    messagingSenderId: "967966406196",
-    appId: "1:967966406196:web:f942c875bd8d59c69dfc61",
-    measurementId: "G-2F2RC5H250"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+
